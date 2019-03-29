9bc85c4c1b4c7bf1c55c22c9ece752e606dc3bc8
Thao <thaotou@amazon.com>
2019-03-28 22:31:03 -0500

    Updates

---
+++ precache-manifest.87000f2bad3b051d492af9ea7c388917.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06c65f6604d562c9f1457276d289cba",
+    "url": "/clicky-car-game/static/media/BMW-M4.b06c65f6.png"
+  },
+  {
+    "revision": "7f5197c5c6d7074e645e",
+    "url": "/clicky-car-game/static/css/main.d779b6e3.chunk.css"
+  },
+  {
+    "revision": "33943e207a4408b6e217",
+    "url": "/clicky-car-game/static/js/runtime~main.b724cd7d.js"
+  },
+  {
+    "revision": "6fff9c48c100cbfcaf66",
+    "url": "/clicky-car-game/static/js/2.34fe02dd.chunk.js"
+  },
+  {
+    "revision": "ac3a5bb2516efb8385b184908f1da38f",
+    "url": "/clicky-car-game/static/media/2019-Mustang-GT350.ac3a5bb2.png"
+  },
+  {
+    "revision": "217589230a3b566f5db6eef66d62f0f8",
+    "url": "/clicky-car-game/static/media/2019-corvette-zr1.21758923.png"
+  },
+  {
+    "revision": "2017e2d810db214daff61cff911a0eff",
+    "url": "/clicky-car-game/static/media/2020-Toyota-Supra.2017e2d8.png"
+  },
+  {
+    "revision": "018091adc4b3d24130386304fc75bd0f",
+    "url": "/clicky-car-game/static/media/AlfaRomeo-Giulia-Quadrifoglio.018091ad.png"
+  },
+  {
+    "revision": "01b52e18b9689e0dbbb0faf002dfc319",
+    "url": "/clicky-car-game/static/media/Audi-RS5.01b52e18.png"
+  },
+  {
+    "revision": "7f5197c5c6d7074e645e",
+    "url": "/clicky-car-game/static/js/main.319bfd42.chunk.js"
+  },
+  {
+    "revision": "6787b72c281d041cd3336085eb06d0dd",
+    "url": "/clicky-car-game/static/media/Chevrolet-Camaro-ZL1.6787b72c.png"
+  },
+  {
+    "revision": "7e0fc78a81a56abf044d6a6e18144bc3",
+    "url": "/clicky-car-game/static/media/Infiniti-Q60.7e0fc78a.png"
+  },
+  {
+    "revision": "3b2f0f14a59fef16203258b952919637",
+    "url": "/clicky-car-game/static/media/Jaguar-F-Type.3b2f0f14.png"
+  },
+  {
+    "revision": "2e141f3ce396d109fb59f1f53d130ed1",
+    "url": "/clicky-car-game/static/media/Lexus-RCF.2e141f3c.png"
+  },
+  {
+    "revision": "78d3d3cebc449c1ab5fcb3853f7976ab",
+    "url": "/clicky-car-game/static/media/MercedesBenz-S63-AMG.78d3d3ce.png"
+  },
+  {
+    "revision": "53b5e7ac692e932d005f90da3694f016",
+    "url": "/clicky-car-game/static/media/Porsche-911.53b5e7ac.png"
+  },
+  {
+    "revision": "e883a9b54c30ed98e37c58d197266a1e",
+    "url": "/clicky-car-game/static/media/RaceTrack.e883a9b5.jpg"
+  },
+  {
+    "revision": "e1db3e7be6f44e901731966a6a28b0d8",
+    "url": "/clicky-car-game/index.html"
+  }
+];
